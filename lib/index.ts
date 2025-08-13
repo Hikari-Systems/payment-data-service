8100dbe5484e1f2c9a4152472fcffbac21aaf96a
Rick Knowles <rick.knowles@hikari-systems.com>
2025-08-13 07:48:12 +0100

    moved timing to be after healthcheck

--- lib/index.ts
+++ lib/index.ts
@@ -4,9 +4,11 @@ import { timingMiddleware } from '@hikari-systems/hs.utils';
 import routes from './route';
 
 const app = express.Router();
-app.use(timingMiddleware);
 
 app.get('/healthcheck', (_req, res) => res.status(200).send('OK'));
+
+app.use(timingMiddleware);
+
 app.use('/api', routes);
 app.use('/', express.static(path.join(__dirname, '../static')));
 
